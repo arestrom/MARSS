69bf3c923d97fead5bda34ac30b87ef6a4e074dc
Are Strom <are.strom@gmail.com>
2021-04-14 11:11:49 -0700

    Incremental

--- survey_query/survey_query_global.R
+++ survey_query/survey_query_global.R
@@ -1,6 +1,6 @@
 
 # Get all creel sites
-get_survey_sites = function(pool) {
+get_survey_query_sites = function(pool) {
   qry = glue("select distinct loc.location_id, loc.location_name, ",
              "loc.location_code || ': ' || loc.location_name as survey_site ",
              "from location as loc ",
@@ -15,7 +15,7 @@ get_survey_sites = function(pool) {
 }
 
 # Samplers
-get_survey_samplers = function(pool) {
+get_survey_query_samplers = function(pool) {
   qry = glue("select sampler_id, first_name, last_name, ",
              "first_name || ' ' || last_name as sampler_name ",
              "from sampler ",
@@ -25,4 +25,54 @@ get_survey_samplers = function(pool) {
     select(sampler_id, sampler_name)
   poolReturn(con)
   return(survey_sampler_list)
+}
+
+# Get all creel sites for selected years and catch areas
+get_query_surveys = function(pool, start_date, end_date, sites, samplers) {
+  qry = glue("select s.survey_id, s.survey_datetime as survey_date, loc.location_name, ",
+             "loc.location_code || ': ' || loc.location_name as survey_site, ",
+             "smp.last_name, smp.first_name, ",
+             "smp.first_name || ' ' || smp.last_name as sampler_name, ",
+             "s.start_datetime as start_time, ",
+             "s.end_datetime as end_time, ",
+             "sdt.survey_design_type_description as survey_design, ",
+             "s.no_effort_indicator as no_effort, ",
+             "s.comment_text as survey_comment, ",
+             "s.created_datetime as created_date, ",
+             "s.created_by, s.modified_datetime as modified_date, ",
+             "s.modified_by ",
+             "from survey as s ",
+             "inner join location as loc on s.location_id = loc.location_id ",
+             "inner join location_type_lut as lt on loc.location_type_id = lt.location_type_id ",
+             "inner join survey_sampler as ss on s.survey_id = ss.survey_id ",
+             "inner join sampler as smp on ss.sampler_id = smp.sampler_id ",
+             "left join survey_design_type_lut as sdt on s.survey_design_type_id = sdt.survey_design_type_id ",
+             "where date(s.survey_datetime::timestamp at time zone 'America/Los_Angeles') >= '{start_date}' ",
+             "and date(s.survey_datetime::timestamp at time zone 'America/Los_Angeles') <= '{end_date}' ",
+             "and st.survey_type_description in ('Puget Sound dockside creel survey') ",
+             "and lt.location_type_description = 'Creel survey site' ",
+             "and s.location_id in ({sites}) and ss.sampler_id in ({samplers}) ",
+             "order by loc.location_name, s.survey_datetime, smp.last_name, smp.first_name")
+  con = poolCheckout(pool)
+  surveys = DBI::dbGetQuery(con, qry)
+  poolReturn(con)
+  surveys = surveys %>%
+    mutate(survey_date = with_tz(survey_date, tzone = "America/Los_Angeles")) %>%
+    mutate(survey_date_dt = format(survey_date, "%m/%d/%Y")) %>%
+    mutate(start_time = with_tz(start_time, tzone = "America/Los_Angeles")) %>%
+    mutate(start_time_dt = format(start_time, "%H:%M")) %>%
+    mutate(end_time = with_tz(end_time, tzone = "America/Los_Angeles")) %>%
+    mutate(end_time_dt = format(end_time, "%H:%M")) %>%
+    mutate(any_effort = if_else(no_effort == TRUE, "No", "Yes")) %>%
+    mutate(created_date = with_tz(created_date, tzone = "America/Los_Angeles")) %>%
+    mutate(created_dt = format(created_date, "%m/%d/%Y %H:%M")) %>%
+    mutate(modified_date = with_tz(modified_date, tzone = "America/Los_Angeles")) %>%
+    mutate(modified_dt = format(modified_date, "%m/%d/%Y %H:%M")) %>%
+    mutate(survey_date = as.Date(survey_date)) %>%
+    select(survey_id, survey_date, survey_date_dt, survey_site,
+           sampler_name, start_time, start_time_dt, end_time, end_time_dt,
+           survey_design, any_effort, survey_comment, created_date, created_dt,
+           created_by, modified_date, modified_dt, modified_by) %>%
+    arrange(survey_date, start_time, end_time, created_date)
+  return(surveys)
 }
--- survey_query/survey_query_srv.R
+++ survey_query/survey_query_srv.R
@@ -2,64 +2,98 @@
 # All sites select
 output$all_sites_select = renderUI({
   req(valid_connection == TRUE)
-  survey_site_list = get_survey_sites(pool)$survey_site
+  survey_site_list = get_survey_query_sites(pool)$survey_site
   pickerInput("all_sites_select",
-              label = "Survey site",
+              label = "Survey site(s)",
               multiple = TRUE,
               choices = survey_site_list,
               selected = survey_site_list[1],
               width = "325px",
-              options = list(pickerOptions(
-                actionsBox = TRUE),
-                "style-base" = "form-control", style = "")
+              options = pickerOptions(
+                actionsBox = TRUE
+              )
   )
 })
 
 # All sampler select
 output$all_samplers_select = renderUI({
   req(valid_connection == TRUE)
-  survey_sampler_list = get_survey_samplers(pool)$sampler_name
+  survey_sampler_list = get_survey_query_samplers(pool)$sampler_name
   pickerInput("all_samplers_select",
-              label = "Sampler name",
+              label = "Sampler name(s)",
               multiple = TRUE,
               choices = survey_sampler_list,
               selected = survey_sampler_list[1],
               width = "225px",
-              options = list(pickerOptions(
-                actionsBox = TRUE),
-                "style-base" = "form-control", style = "")
+              options = pickerOptions(
+                actionsBox = TRUE
+              )
   )
 })
 
-# # Primary DT datatable for samplers
-# output$samplers = renderDT({
+# # All sampler select
+# output$all_samplers_select = renderUI({
 #   req(valid_connection == TRUE)
-#   sampler_title = glue("All samplers currently listed in the database")
-#   sampler_data = get_samplers(pool) %>%
-#     select(first_name, last_name, active, created_dt, created_by,
-#            modified_dt, modified_by)
-#
-#   # Generate table
-#   datatable(sampler_data,
-#             colnames = c("First name", "Last name", "Active?", "Create DT",
-#                          "Create By", "Mod DT", "Mod By"),
-#             selection = list(mode = 'single'),
-#             options = list(dom = 'lftp',
-#                            pageLength = 20,
-#                            lengthMenu = c(20, 50, 100, 500, 2000),
-#                            scrollX = T,
-#                            initComplete = JS(
-#                              "function(settings, json) {",
-#                              "$(this.api().table().header()).css({'background-color': '#9eb3d6'});",
-#                              "}")),
-#             caption = htmltools::tags$caption(
-#               style = 'caption-side: top; text-align: left; color: black; width: auto;',
-#               htmltools::em(htmltools::strong(sampler_title))))
+#   survey_sampler_list = get_survey_query_samplers(pool)$sampler_name
+#   pickerInput("all_samplers_select",
+#               label = "Sampler name(s)",
+#               multiple = TRUE,
+#               choices = survey_sampler_list,
+#               selected = survey_sampler_list[1],
+#               width = "225px",
+#               options = list(pickerOptions(
+#                 actionsBox = TRUE),
+#                 "style-base" = "form-control", style = "")
+#   )
 # })
-#
-# # Create surveys DT proxy object
-# samplers_dt_proxy = dataTableProxy(outputId = "samplers")
-#
+
+# Primary DT datatable for samplers
+output$surveys_query = renderDT({
+  req(valid_connection == TRUE)
+  req(input$survey_date_range)
+  req(input$all_sites_select)
+  req(input$all_samplers_select)
+  start_date = input$survey_date_range[1]
+  end_date = input$survey_date_range[2]
+  selected_sites = input$all_sites_select
+  site_ids = get_survey_query_sites(pool) %>%
+    filter(survey_site %in% selected_sites) %>%
+    distinct() %>%
+    pull(location_id)
+  sites = paste0(paste0("'", site_ids, "'"), collapse = "' ")
+  selected_samplers = input$all_samplers_select
+  sampler_ids = get_survey_query_samplers(pool) %>%
+    filter(sampler_name %in% selected_samplers) %>%
+    distinct() %>%
+    pull(sampler_id)
+  samplers = paste0(paste0("'", sampler_ids, "'"), collapse = ", ")
+  query_title = glue("All surveys for selected dates, sites, and samplers")
+  query_data = get_query_surveys(pool, start_date, end_date, sites, samplers)  %>%
+    select(survey_date_dt, survey_site, sampler_name, start_time_dt, end_time_dt,
+           survey_design, any_effort, survey_comment, created_dt, created_by,
+           modified_dt, modified_by)
+
+  # Generate table
+  datatable(query_data,
+            colnames = c("First name", "Last name", "Active?", "Create DT",
+                         "Create By", "Mod DT", "Mod By"),
+            selection = list(mode = 'single'),
+            options = list(dom = 'lftp',
+                           pageLength = 20,
+                           lengthMenu = c(20, 50, 100, 500, 2000),
+                           scrollX = T,
+                           initComplete = JS(
+                             "function(settings, json) {",
+                             "$(this.api().table().header()).css({'background-color': '#9eb3d6'});",
+                             "}")),
+            caption = htmltools::tags$caption(
+              style = 'caption-side: top; text-align: left; color: black; width: auto;',
+              htmltools::em(htmltools::strong(sampler_title))))
+})
+
+# Create surveys DT proxy object
+samplers_dt_proxy = dataTableProxy(outputId = "samplers")
+
 # #========================================================
 # # Collect sampler values from selected row for later use
 # #========================================================
